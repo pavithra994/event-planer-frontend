14391793e794552cd38be9544bee2b2ad158ad76
t2mrnd <pavithra.m@shinebridgesolutions.com>
2020-04-20 23:15:09 +0530

    styling

--- src/app/components/event-calendar/event-calendar.component.ts
+++ src/app/components/event-calendar/event-calendar.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import {MatCalendarCellCssClasses} from '@angular/material/datepicker';
-import {EventManageService} from '../../event-manage.service';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatCalendarCellCssClasses, MatCalendar } from '@angular/material/datepicker';
+import { EventManageService } from '../../event-manage.service';
 
 @Component({
   selector: 'app-event-calendar',
@@ -8,32 +8,28 @@ import {EventManageService} from '../../event-manage.service';
   styleUrls: ['./event-calendar.component.css']
 })
 export class EventCalendarComponent implements OnInit {
+
+  @ViewChild(MatCalendar) calendar;
   selectedDate: any;
   eventArray;
-  datesToHighlight = ['2020/04/26',
-    '2020/04/26',
-    '2020/04/26',
-    ];
+  loadingItem = false;
   constructor(private eventManageService: EventManageService) { }
 
   ngOnInit(): void {
     this.eventManageService.getInstance().subscribe(value => {
-      console.log(value);
+      this.loadingItem = true;
       this.eventArray = value;
+      setTimeout(() => {
+        this.loadingItem = false;
+      }, 100);
     });
   }
 
-  dateClass() {
-    // tslint:disable-next-line:variable-name
-    return (date_m: Date): MatCalendarCellCssClasses => {
-      const date = new Date(date_m)
-      // const highlightDate = this.datesToHighlight
-      //   .map(strDate => new Date(strDate))
-      //   .some(d => d.getDate() === date.getDate() && d.getMonth() === date.getMonth() && d.getFullYear() === date.getFullYear());
-      const highlightDate = this.eventManageService.getEventList()
-        .map(strDate => strDate.eventTime)
-        .some(d => d.getDate() === date.getDate() && d.getMonth() === date.getMonth() && d.getFullYear() === date.getFullYear());
-      return highlightDate ? 'special-date' : '';
-    };
+  dateClass = (currDate: Date) => {
+    const date = new Date(currDate);
+    const highlightDate = this.eventArray
+      .map(strDate => strDate.eventTime)
+      .some(d => d.getDate() === date.getDate() && d.getMonth() === date.getMonth() && d.getFullYear() === date.getFullYear());
+    return highlightDate ? 'special-date' : '';
   }
 }
